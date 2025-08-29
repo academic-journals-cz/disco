919adba35213dbfabbce890cbbd4a76e76562591
munipress <gomola@press.muni.cz>
2025-08-29 09:08:16 +0200

    delete DDH disclamers - moved to JMEF plugin

--- DiscoPlugin.inc.php
+++ DiscoPlugin.inc.php
@@ -134,8 +134,6 @@ class DiscoPlugin extends GenericPlugin {
 
             HookRegistry::register('Templates::Common::Footer::PageFooter', array($this, 'callbackTemplateCommonPageFooter'));
 
-            HookRegistry::register('TemplateManager::display', array($this, 'addDiamondTexts'));
-
             // Register the components this plugin implements to
             // permit administration of disco.
             HookRegistry::register('LoadComponentHandler', array($this, 'setupHandler'));
@@ -143,54 +141,6 @@ class DiscoPlugin extends GenericPlugin {
             HookRegistry::register('LoadHandler', array($this, 'callbackHandleContent'));
         }
         return true;
-    }
-
-    public function addDiamondTexts($hookName, $args) {
-        $templateMgr = $args[0];
-        $template = $args[1];
-
-        $request = Application::get()->getRequest();
-        $context = $request->getContext();
-        $contextId = $context->getId();
-
-        // Get the disco settings
-        $discoDao = DAORegistry::getDAO('DiscoDAO');
-        $discoIterator = $discoDao->getByContextId($contextId);
-        $disco = $discoIterator->next();
-
-        if ($template !== "frontend/pages/about.tpl")
-            return false;
-
-        // Get "about" page content
-        $currentContext = $templateMgr->getTemplateVars('currentContext');
-        $currentLocale = AppLocale::getLocale();
-
-        if ($currentContext) {
-            $aboutText = $currentContext->getLocalizedSetting('about');
-
-            // Add own text to about context part
-
-            if ((bool) $disco->getOpenAuthorship()) {
-                $aboutText .= __('plugins.generic.disco.about.openToAllAuthors', array('contextTitle' => $currentContext->getLocalizedData('name')));
-            }
-            if ((bool) $disco->getOwnershipScience()) {
-                if ($disco->getOrganisationType() == "nonprofit") {
-                    $organisationType = __('plugins.generic.disco.diamond.organisationType.nonProfit');
-                } else {
-                    $organisationType = __('plugins.generic.disco.diamond.organisationType.public');
-                }
-                $aboutText .= __('plugins.generic.disco.about.communityOwned', array('contextTitle' => $currentContext->getLocalizedData('name'), 'publisherInstitution' => $currentContext->getData('publisherInstitution'), 'organisationType' => $organisationType));
-            }
-            // Content update inside object
-            $currentContext->setData('about', $aboutText, $currentLocale);
-        }
-
-        // Assign whole updated object to template
-        $templateMgr->assign(array(
-            'currentContext' => $currentContext,
-        ));
-
-        return false;
     }
 
     /**
@@ -917,7 +867,6 @@ class DiscoPlugin extends GenericPlugin {
         $variables['journalPublisherNameAvailable'] = (bool) $disco->getJournalPublisherNameAvailable();
         $variables['badgesAvailable'] = (bool) $disco->getBadgesAvailable();
 
-        $variables['organisationType'] = $disco->getOrganisationType();
         return $variables;
     }
 
