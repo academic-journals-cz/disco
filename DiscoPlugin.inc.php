8dff706aaf497055455e908df420bf943123fe19
munipress <gomola@press.muni.cz>
2026-01-26 11:40:48 +0100

    De-duplicate eISSN

--- DiscoPlugin.inc.php
+++ DiscoPlugin.inc.php
@@ -534,7 +534,6 @@ class DiscoPlugin extends GenericPlugin {
             "fullContentAvailable" => array(SERVICE_WOS_ESCI),
             "functionalWebsite" => array(SERVICE_WOS_ESCI),
             "journalUrl" => array(SERVICE_WOS_ESCI, SERVICE_DOAJ, SERVICE_PUBMED),
-            "eIssn" => array(SERVICE_WOS_ESCI, SERVICE_SCOPUS, SERVICE_ERIHPLUS, SERVICE_REDALYC, SERVICE_DOAJ),
             "journalTitle" => array(SERVICE_WOS_ESCI, SERVICE_REDALYC),
             "qualityEnHomepage" => array(SERVICE_SCOPUS),
         );
